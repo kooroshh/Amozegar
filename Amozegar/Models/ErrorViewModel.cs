6b96f0e62fd81b217d8d3b3a84de918a9261104d
Kooroshh <kooroshsoleymani98@gmail.com>
2025-04-24 14:16:02 +0330

    Add Project With Landing Page To Git

---
+++ Amozegar/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Amozegar.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
