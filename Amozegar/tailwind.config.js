65f3793c98b43c860db6b023b21d038f127f9f18
Kooroshh <kooroshsoleymani98@gmail.com>
2025-06-01 15:13:15 +0330

    Add for frontend

---
+++ Amozegar/tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: [
+        "./Views/**/*.cshtml",
+        "./Areas/**/*.cshtml",
+        "./wwwroot/**/*.js"
+    ],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+}
